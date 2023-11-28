add19079fb30a7cb6f23353f91a9bd77ce26e552
Ismael Freire <brandonfreire@yahoo.com>
2023-11-28 11:50:04 -0500

    clase 9

--- CadenaUtilities.java
+++ CadenaUtilities.java
@@ -1,17 +1,18 @@
-import java.util.jar.Attributes.Name;
+//import java.lang.foreign.ValueLayout.OfInt;
+//import java.util.jar.Attributes.Name;
 
 public class CadenaUtilities {
     public static void main(String[] args) {
         // Declaraciones
         byte asci[] = {71,70,71,72,73,75};
-        byte characters[] = {'G', 'f', 'g'};
+        byte character[] = {'G', 'f', 'g'};
         String cad = new String(asci);
         System.out.println("Cadena original: " + cad);
         //Para escoger hasta que posicion de la cadena asci se quiere
         String cad2 = new String(asci,2,2);
         System.out.println("Cadena con posicion y longitud: " + cad2);
         //
-        String cad3 = new String(characters);
+        String cad3 = new String(character);
         System.out.println("cadena de caracteres: " + cad3);
 
         String str = new String(cad3);
@@ -22,16 +23,44 @@ public class CadenaUtilities {
         int len = str.length();
         System.out.println("Longitud de la cadena: "+len);
 
-        char  c = str,charAt(1);
+        Character  c = str.charAt(1);
         String s = c.toString();
         System.out.println("chatAt(): El caracter en el indice 1 es: "+c);
         System.out.println("c.tostring(): El caracter en el indice 1 es: "+s);
-        
+
+        boolean b = str.equals("GFG");
+        System.out.println("equalsIgnoreCase(): La cadena es igual a \"GFG\": " +b);
+
+        str.concat(".abc");
+        str.concat(".123");
+        str.concat(".*---+");
+        System.out.println("concat(): La concatenación es: " + str);
 
         
 
+        String subStr123 = str.substring(8, 11);
+        System.out.println("substring(): Substring del indice 8 al 3: "+subStr123);
 
-        //getDemoString();
+        String subStr1 = str.substring(4,str.length());
+        System.out.println("substring(): substring desde indice 4 hasta el final: " +subStr1);
+
+        String subStr2 = str.replace('a', 'A');
+        System.out.println("replace(): Reemplazar los valores de A por mayuscula: "+subStr2);
+
+        String subStr3 = str.toUpperCase();
+        System.out.println("toUpperCase(): La conversion a mayusculas es: " +subStr3);
+
+        String subStr4 = str.toLowerCase();
+        System.out.println(" toLowerCase(): Convertir todos los caracteres a minusculas" +subStr4);
+
+        //Para hacer divisiones 
+        String[] strSplit = str.split(".");    // gfg abc 123 *--+ abc
+        //for (int i = 0; i<strSplit.length; i++)
+        System.out.println(" str.split(\".\"): dividir la cadena segun el punto: " +strSplit);
+        for (String subS : strSplit) {                // acaba de crear 5 segmentos del array
+            System.out.print(subS+ " ");              // for "inteligente", tiene tres puntos automaticos
+        }
+        
     }
 
     private static void getDemoString() {
