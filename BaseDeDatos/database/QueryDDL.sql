41b88c3a6429aa9e504ba9f1afb4e7f8b1f260d1
Ismael <brandon.freire@outlook.com>
2024-02-06 20:47:06 -0500

    base de datos

--- BaseDeDatos/database/QueryDDL.sql
+++ BaseDeDatos/database/QueryDDL.sql
@@ -6,4 +6,53 @@ CREATE TABLE Raza(
     ,FechaMod        DATE
 );
 
+CREATE TABLE Sexo(
+    IDSexo          INTEGER  PRIMARY KEY AUTOINCREMENT 
+    ,Descripcion     TEXT NOT NULL
+    ,Observacion     TEXT
+    ,FechaCrea       DATE NOT NULL
+    ,FechaMod        DATE
+);
+
+CREATE TABLE TipoMascota(
+    IDTipoMascota          INTEGER  PRIMARY KEY AUTOINCREMENT 
+    ,Descripcion     TEXT NOT NULL
+    ,Observacion     TEXT
+    ,FechaCrea       DATE NOT NULL
+    ,FechaMod        DATE
+);
+
+CREATE TABLE Mascota(
+    IDMascota          INTEGER  PRIMARY KEY AUTOINCREMENT 
+    ,Descripcion     TEXT NOT NULL
+    ,Observacion     TEXT
+    ,FechaCrea       DATE NOT NULL
+    ,FechaMod        DATE
+);
+
+
+INSERT  INTO Sexo (Descripcion, Observacion, FechaCrea, FechaMod)
+VALUES ("Macho", "sin/obs","06/02/2024",""), 
+("Hembra", "sin/obs","06/02/2024","");
+
+INSERT  INTO Raza (Descripcion, Observacion, FechaCrea, FechaMod)
+VALUES ("Pug", "sin/obs","06/02/2024",""), 
+("Bulldog", "sin/obs","06/02/2024",""),
+("Delmer", "sin/obs","06/02/2024",""),
+("French Poodle", "sin/obs","06/02/2024","");
+
+INSERT  INTO TipoMascota (Descripcion, Observacion, FechaCrea, FechaMod)
+VALUES ("Perro", "sin/obs","06/02/2024",""), 
+("Gato", "sin/obs","06/02/2024","");
+
+INSERT  INTO Mascota (Descripcion, Observacion, FechaCrea, FechaMod)
+VALUES ("Focha", "sin/obs","06/02/2024",""), 
+("Gordo", "sin/obs","06/02/2024",""),
+("Tadeo", "sin/obs","06/02/2024",""),
+("Firulais", "sin/obs","06/02/2024","");
+
+
+SELECT * FROM Sexo;
+SELECT * FROM TipoMascota;
+SELECT * FROM Mascota;
 SELECT * FROM Raza;
